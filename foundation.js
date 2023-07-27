d67311c121410be7dc92689d200d684c8efd2290
Saida <muxtarovamunisabonu@gmail.com>
2023-07-27 18:35:49 +0500

    sorted objects

---
+++ foundation.js
@@ -0,0 +1,95 @@
+// let yosh = 20
+
+// if(yosh > 18){
+//     console.log('hush kelibsiz')
+
+// }else{
+//     console.log("vohli")
+// }
+
+// let yosh = 18
+// switch (yosh) {
+//   case 18:
+//     console.log("voyaga yetgan");
+//     break;
+
+//   case 40:
+//     console.log("orta yosh");
+//     break;
+
+//   default:
+//     console.log("qari");
+// }
+
+// stack = [];
+// function isEmpty() {
+//   if (stack.length == 0) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isEmpty());
+
+// stack = [];
+// function Push(element) {
+//   stack.push(element);
+// }
+// Push(10);
+// Push(11);
+// Push(12);
+// console.log(stack);
+
+// stack = [1,2,3,4];
+// function Pop() {
+//   if (stack.length == 0) {
+//     return "bo'sh";
+//   }
+//   return stack.pop();
+// }
+// console.log(Pop());
+// console.log(stack)
+
+
+
+// stack = [55,77,88];
+// function Peek() {
+//   if (stack.length == 0) {
+//     return "bo'sh";
+//   }
+//   return stack[stack.length - 1];
+// }
+// console.log(Peek());
+
+// stack = [33,44,55,77,88,99,100];
+// function Size() {
+//   if (stack.length == 0) {
+//     return "bo'sh";
+//   }
+//   return stack.length;
+// }
+// console.log(Size());
+
+// stack = [12, 33];
+// function Enqueue(element) {
+//   stack.push(element);
+// }
+// Enqueue(39);
+// console.log(stack);
+
+// stack = [1, 2, 3];
+// function Dequeue() {
+//   if (stack.length == 0) {
+//     return "bo'sh";
+//   }
+//   return stack.shift();
+// }
+// console.log(Dequeue());
+
+// stack = [1,2,3];
+// function Front() {
+//   if (stack.length == 0) {
+//     return "bo'sh";
+//   }
+//   return stack[0];
+// }
+// console.log(Front());
